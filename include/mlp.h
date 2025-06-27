a09773aad70a5cdc815ba7437899584ac1f5d69f
Leonel <leonelmmorgado@gmail.com>
2025-06-27 18:10:24 -0300

    add activation functions

--- include/mlp.h
+++ include/mlp.h
@@ -6,27 +6,28 @@
 typedef struct _mlp_layer {
     uint16_t input_size;
     uint16_t output_size;
-    float *weights;
-    float *biases;
-    float *outputs;
-    float *deltas;
+    double *weights;
+    double *biases;
+    double *outputs;
+    double *deltas;
 } MLPLayer;
 
 typedef enum _forward_func_enum {
     UNKNOWN,
     RELU,
     SIGMOID,
+    TANH,
     //TODO: add more support
 } ForwardFunc;
 
 typedef struct _mlp {
     uint8_t num_layers;
     MLPLayer *layers;
-    ForwardFunc forward_func;
+    double (*func)(double);
 } MLP;
 
-void forward(MLP *mlp, float *inp);
-void backward(); //TODO: add
-void create_mlp(MLP *mlp, uint16_t *layer_size, uint8_t num_layers);
+void forward(MLP *mlp, double *inp);
+void back(); //TODO: add
+void create_mlp(MLP *mlp, uint16_t *layer_size, uint8_t num_layers, ForwardFunc func_type);
 
 #endif
--- src/mlp.c
+++ src/mlp.c
@@ -1,26 +1,52 @@
 #include <stdlib.h>
 #include <stdint.h>
+#include <math.h>
 #include "mlp.h"
 
-float randf() {
-    return rand()/RAND_MAX;
+doble randd() {
+    return (double)rand()/RAND_MAX;
 }
 
 void init_layer(MLPLayer *layer, uint16_t inp_size, uint16_t out_size) {
     layer->input_size;
     layer->output_size;
-    layer->weights = malloc(sizeof(float) * inp_size * out_size);
-    layer->biases = calloc(out_size, sizeof(float));
-    layer->outputs = calloc(out_size, sizeof(float));
-    layer->deltas = calloc(out_size, sizeof(float));
+    layer->weights = malloc(sizeof(double) * inp_size * out_size);
+    layer->biases = calloc(out_size, sizeof(double));
+    layer->outputs = calloc(out_size, sizeof(double));
+    layer->deltas = calloc(out_size, sizeof(double));
     for(int i = 0; i < inp_size * out_size; i++)
-        layer->weights[i] = randf() * 0.5;
+        layer->weights[i] = randd() * 0.5;
+}
+
+double relu(double x) {
+    return x > 0 ? x : 0;
+}
+
+double sigmoid(double x) {
+    double etx = exp(x);//e^x
+    double oneup = etx+1;
+    return etx/oneup;
+}
+
+double tanh(double x) {
+    return tanh(x);
 }
 
 void create_mlp(MLP *mlp, uint16_t *layer_size, uint8_t num_layers, ForwardFunction function_type) {
     mlp->num_layers = num_layers - 1;
     mlp->layers = malloc(sizeof(MLPLayer) * num_layers);
+    if(!mlp->layers) return;
     for(uint8_t i = 0; i < mlp->num_layers; i++)
         init_layer(&mlp->layers[i], layer_size[i], layer_size[i + 1]);
-    mlp->forward_func = function_type;
+    switch(function_type) {
+        case RELU:
+            mlp->func = &relu;
+            break;
+        case SIGMOID:
+            mlp->func = &sigmoid;
+            break;
+        case TANH:
+            mlp->func = &tanh;
+            break;
+    }
 }
